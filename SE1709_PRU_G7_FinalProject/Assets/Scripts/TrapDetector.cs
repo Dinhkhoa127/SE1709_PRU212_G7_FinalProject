ed85f66e66a27eda00654c842900809125ad8274
NgBaNhan <nhan23563@gmail.com>
2025-07-09 14:48:21 +0700

    Skill player

---
+++ SE1709_PRU_G7_FinalProject/Assets/Scripts/TrapDetector.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class TrapDetector : MonoBehaviour
+{
+    private bool isDead = false;
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        if (isDead) return;
+        if (collider.CompareTag("Player"))
+        {
+            isDead = true;
+            // Gọi đúng script PlayerKnight và dùng Die()
+            PlayerKnight player = collider.GetComponent<PlayerKnight>();
+            if (player != null)
+            {
+                player.SendMessage("Die");
+            }
+        }
+    }
+}
